fb17942294b7405f9fb639c3d85ee9afa940bcf7
stanislawbartkowski <stanislawbartkowski@fcd2c1aa-8a59-11de-b841-6521bc8c149b>
2010-02-21 10:39:30 +0000

    changes

---
+++ trunk/javahotel/src/gwtclient/com/gwtmodel/table/login/ValidateLogin.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2010 stanislawbartkowski@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gwtmodel.table.login;
+
+import com.gwtmodel.table.IBackValidate;
+import com.gwtmodel.table.IDataType;
+import com.gwtmodel.table.IVField;
+import com.gwtmodel.table.IVModelData;
+import com.gwtmodel.table.InvalidateFormContainer;
+import com.gwtmodel.table.InvalidateMess;
+import com.gwtmodel.table.factories.IDataValidateAction;
+import com.gwtmodel.table.rdef.FormLineContainer;
+import com.gwtmodel.table.slotmodel.AbstractSlotContainer;
+import com.gwtmodel.table.slotmodel.DataActionEnum;
+import com.gwtmodel.table.slotmodel.GetActionEnum;
+import com.gwtmodel.table.slotmodel.ISlotSignalContext;
+import com.gwtmodel.table.slotmodel.ISlotSignaller;
+import com.gwtmodel.table.view.ValidateUtil;
+import com.gwtmodel.table.view.util.FormUtil;
+import java.util.List;
+
+/**
+ *
+ * @author stanislaw.bartkowski@gmail.com
+ */
+public class ValidateLogin extends AbstractSlotContainer implements
+        IDataValidateAction {
+
+    public interface IValidateLogin {
+
+        void validate(IVModelData vData, IBackValidate backValidate);
+    }
+    private final IDataType dType;
+    private final FormLineContainer fContainer;
+    private final IValidateLogin iValidate;
+
+    public ValidateLogin(IDataType dType, FormLineContainer fContainer, IValidateLogin iValidate) {
+        this.dType = dType;
+        this.fContainer = fContainer;
+        this.iValidate = iValidate;
+        registerSubscriber(DataActionEnum.ValidateAction, dType,
+                new ValidateA());
+    }
+
+    private class Validate implements IBackValidate {
+
+        public void invalid(InvalidateFormContainer errMess) {
+            publish(DataActionEnum.ChangeViewFormToInvalidAction, dType, errMess);
+        }
+
+        public void valid() {
+            publish(DataActionEnum.ValidSignal, dType);
+        }
+    }
+
+    private class ValidateA implements ISlotSignaller {
+
+        public void signal(ISlotSignalContext slContext) {
+            IVModelData pData = getGetterIVModelData(
+                    GetActionEnum.GetViewComposeModelEdited, dType);
+            List<IVField> listMFie = FormUtil.getVList(fContainer);
+            List<InvalidateMess> errMess = ValidateUtil.checkEmpty(pData,
+                    listMFie);
+            if (errMess != null) {
+                publish(DataActionEnum.ChangeViewFormToInvalidAction, dType,
+                        new InvalidateFormContainer(errMess));
+                return;
+            }
+            iValidate.validate(pData, new Validate());
+        }
+    }
+
+    public void startPublish(int cellId) {
+    }
+}
