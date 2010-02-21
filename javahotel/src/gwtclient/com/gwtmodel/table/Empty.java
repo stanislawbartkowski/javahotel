78f5befa0adf7e2126eb55efad05190da130fcab
stanislawbartkowski <stanislawbartkowski@fcd2c1aa-8a59-11de-b841-6521bc8c149b>
2010-02-21 10:39:30 +0000

    changes

---
+++ javahotel/src/gwtclient/com/gwtmodel/table/Empty.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010 stanislawbartkowski@gmail.com 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.gwtmodel.table;
+
+/**
+ *
+ * @author stanislaw.bartkowski@gmail.com
+ */
+public class Empty {
+    
+    private Empty() {
+        
+    }
+
+    private static class EmptyDataType implements IDataType {
+
+        public boolean eq(IDataType dType) {
+            return true;
+        }
+    }
+
+    private static class EmptyFieldType implements IVField {
+
+        public boolean eq(IVField o) {
+            return true;
+        }
+    }
+    private static IDataType eType;
+    private static IVField eField;
+
+    static {
+        eType = new EmptyDataType();
+        eField = new EmptyFieldType();
+    }
+
+    public static IVField getFieldType() {
+        return eField;
+    }
+
+    public static IDataType getDataType() {
+        return eType;
+    }
+}
---
+++ javahotel/src/gwtclient/com/gwtmodel/table/stackpanelcontroller/StackPanelControllerFactory.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2010 stanislawbartkowski@gmail.com 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+package com.gwtmodel.table.stackpanelcontroller;
+
+import com.google.inject.Inject;
+import com.gwtmodel.table.view.stack.StackButton;
+import com.gwtmodel.table.view.stack.StackPanelFactory;
+import java.util.List;
+
+/**
+ *
+ * @author stanislaw.bartkowski@gmail.com
+ */
+public class StackPanelControllerFactory {
+
+    private final StackPanelFactory paFactory;
+
+    @Inject
+    public StackPanelControllerFactory(StackPanelFactory paFactory) {
+        this.paFactory = paFactory;
+    }
+
+    public IStackPanelController construct(List<StackButton> bList) {
+        return new StackPanelController(paFactory, bList);
+    }
+}
