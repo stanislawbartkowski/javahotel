9c1898dd546febb5cd460d56d96c4643cc6df953
stanislawbartkowski@gmail.com <stanislawbartkowski@gmail.com@fcd2c1aa-8a59-11de-b841-6521bc8c149b>
2010-11-19 16:08:53 +0000

    small changes

--- trunk/javahotel/src/hotelejb3jpa/com/javahotel/db/hotelbase/jpa/OfferSeasonPeriod.java
+++ trunk/javahotel/src/hotelejb3jpa/com/javahotel/db/hotelbase/jpa/OfferSeasonPeriod.java
@@ -16,15 +16,12 @@ import java.util.Date;
 
 import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import com.javahotel.common.toobject.SeasonPeriodT;
-import com.javahotel.db.jtypes.IId;
 
 /**
  *
