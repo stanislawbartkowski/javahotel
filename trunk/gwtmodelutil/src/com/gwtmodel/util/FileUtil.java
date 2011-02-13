4011323104876a437efd19716d7fc889599650d4
stanislawbartkowski <stanislawbartkowski@fcd2c1aa-8a59-11de-b841-6521bc8c149b>
2011-02-13 23:15:37 +0000

    GWT 2.2 Presentaton : sort by column

--- trunk/gwtmodelutil/src/com/gwtmodel/util/FileUtil.java
+++ trunk/gwtmodelutil/src/com/gwtmodel/util/FileUtil.java
@@ -30,53 +30,51 @@ import org.xml.sax.SAXException;
 
 public class FileUtil {
 
-	public static File getResourceDir(Class cl) {
-		String me = cl.getName().replace(".", "/") + ".class";
-		URL u = cl.getClassLoader().getResource(me);
-		String p = u.getFile();
-		File f = new File(p);
-		String s = f.getParent();
-		return new File(s + File.separatorChar + "resources");
-	}
+    public static File getResourceDir(Class cl) {
+        String me = cl.getName().replace(".", "/") + ".class";
+        URL u = cl.getClassLoader().getResource(me);
+        String p = u.getFile();
+        File f = new File(p);
+        String s = f.getParent();
+        return new File(s + File.separatorChar + "resources");
+    }
 
-	public static URL getResourceURL(Class cl, String pa) {
-		URL u;
-		u = cl.getClassLoader().getResource("resources/" + pa);
-		return u;
-	}
+    public static URL getResourceURL(Class cl, String pa) {
+        URL u;
+        u = cl.getClassLoader().getResource("resources/" + pa);
+        return u;
+    }
 
-	public static Schema getSchema(Class cl, String pa) throws SAXException {
-		URL u = getResourceURL(cl, pa + ".xsd");
-		SchemaFactory fa = SchemaFactory
-				.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		Schema se = fa.newSchema(u);
-		return se;
-	}
+    public static Schema getSchema(Class cl, String pa) throws SAXException {
+        URL u = getResourceURL(cl, pa + ".xsd");
+        SchemaFactory fa = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema se = fa.newSchema(u);
+        return se;
+    }
 
-	public static void validate(Class cl, String xmlName, String schemaName)
-			throws SAXException, IOException {
-		Schema se = getSchema(cl, schemaName);
-		Validator validator = se.newValidator();
-		validator.validate(new StreamSource(xmlName));
+    public static void validate(Class cl, String xmlName, String schemaName)
+            throws SAXException, IOException {
+        Schema se = getSchema(cl, schemaName);
+        Validator validator = se.newValidator();
+        validator.validate(new StreamSource(xmlName));
 
-	}
+    }
 
-	public static Map<String, String> PropertyToMap(Properties pro) {
-		Map<String, String> map = new HashMap<String, String>();
-		for (Entry<Object, Object> e : pro.entrySet()) {
-			String key = (String) e.getKey();
-			String val = (String) e.getValue();
-			map.put(key, val);
-		}
-		return map;
-	}
+    public static Map<String, String> PropertyToMap(Properties pro) {
+        Map<String, String> map = new HashMap<String, String>();
+        for (Entry<Object, Object> e : pro.entrySet()) {
+            String key = (String) e.getKey();
+            String val = (String) e.getValue();
+            map.put(key, val);
+        }
+        return map;
+    }
 
-	public static Properties MapToProperty(Map<String, String> ma) {
-		Properties prop = new Properties();
-		for (Entry<String, String> e : ma.entrySet()) {
-			prop.put(e.getKey(), e.getValue());
-		}
-		return prop;
-	}
-
+    public static Properties MapToProperty(Map<String, String> ma) {
+        Properties prop = new Properties();
+        for (Entry<String, String> e : ma.entrySet()) {
+            prop.put(e.getKey(), e.getValue());
+        }
+        return prop;
+    }
 }
