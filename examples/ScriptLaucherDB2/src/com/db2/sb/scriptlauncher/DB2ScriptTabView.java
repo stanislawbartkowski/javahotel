8cab0370712eba81c617c65a81a2084559fadeb6
stanislawbartkowski@gmail.com <stanislawbartkowski@gmail.com@fcd2c1aa-8a59-11de-b841-6521bc8c149b>
2011-10-07 20:29:00 +0000

    First checkin

---
+++ examples/ScriptLaucherDB2/src/com/db2/sb/scriptlauncher/DB2ScriptTabView.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2011 stanislawbartkowski@gmail.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.db2.sb.scriptlauncher;
+
+import java.io.IOException;
+
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.internal.ui.SWTFactory;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaLaunchTab;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Text;
+
+public class DB2ScriptTabView extends JavaLaunchTab {
+
+	private Text db2Alias;
+	private Text db2User;
+	private Text db2Password;
+
+	public final static String DBACCESS_ID = "com.db2.sb.scriptlauncher.DB2_ACCESS"; //$NON-NLS-1$
+
+	public final static String DB_SCRIPT_TO_RUN = "com.db2.sb.scriptlauncher.DB_SCRIPT_TO_RUN"; //$NON-NLS-1$
+	public final static String DB_CONTENT_TO_RUN = "com.db2.sb.scriptlauncher.DB_CONTENT_TO_RUN"; //$NON-NLS-1$
+
+	private final static String DBNAME_LABEL = Messages.DB2ScriptTabView_DB2_ALIAS_NAME;
+	private final static String DB_USER = Messages.DB2ScriptTabView_DB2_USER_NAME;
+	private final static String DB_PASSWORD = Messages.DB2ScriptTabView_DB2_PASSWORD;
+
+	@Override
+	public void createControl(final Composite parent) {
+
+		Composite comp = SWTFactory.createComposite(parent, 1, 1,
+				GridData.CENTER);
+		setControl(comp);
+		// Composite namecomp = SWTFactory.createComposite(comp, comp.getFont(),
+		// 4, 1, GridData.FILL_HORIZONTAL, 0, 0);
+		Group namecomp = SWTFactory.createGroup(comp, Messages.DB2ScriptTabView_DB2_ACCESS_DATA, 4, 1,
+				GridData.FILL_HORIZONTAL);
+
+		SWTFactory.createLabel(namecomp, DBNAME_LABEL, 1);
+		db2Alias = SWTFactory.createSingleText(namecomp, 1);
+
+		// namecomp = SWTFactory.createComposite(comp, comp.getFont(), 4, 1,
+		// GridData.FILL_HORIZONTAL, 0, 0);
+
+		SWTFactory.createLabel(namecomp, DB_USER, 1);
+		db2User = SWTFactory.createSingleText(namecomp, 1);
+
+		// namecomp = SWTFactory.createComposite(comp, comp.getFont(), 4, 1,
+		// GridData.FILL_HORIZONTAL, 0, 0);
+
+		SWTFactory.createLabel(namecomp, DB_PASSWORD, 1);
+		db2Password = SWTFactory.createSingleText(namecomp, 1);
+
+		Composite bcomp = SWTFactory.createComposite(comp, comp.getFont(), 4,
+				1, GridData.FILL_HORIZONTAL, 0, 0);
+		Button b = SWTFactory.createPushButton(bcomp, Messages.DB2ScriptTabView_DB2_BUTTON_TEST, null);
+
+		b.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				try {
+					DB2LineFactory.executeDB2Script(parent.getShell(), null,
+							db2Alias.getText(), db2User.getText(),
+							db2Password.getText(), null, null);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+	}
+
+	@Override
+	public String getName() {
+		return Messages.DB2ScriptTabView_DB2_LAUNCHER_NAME;
+	}
+
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy arg0) {
+		DB2AccessData a = DB2AccessData.contructDefa();
+		arg0.setAttribute(DBACCESS_ID, a.getParamList());
+	}
+
+	@Override
+	public void initializeFrom(ILaunchConfiguration config) {
+		DB2AccessData a = DB2AccessData.construct(config);
+		db2Alias.setText(a.getDb2Alias());
+		db2User.setText(a.getDb2User());
+		db2Password.setText(a.getDb2Password());
+	}
+
+	@Override
+	public boolean canSave() {
+		return true;
+	}
+
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		DB2AccessData a = DB2AccessData.contructDefa();
+		a.setDb2Alias(db2Alias.getText());
+		a.setDb2User(db2User.getText());
+		a.setDb2Password(db2Password.getText());
+		configuration.setAttribute(DBACCESS_ID, a.getParamList());
+	}
+
+}
