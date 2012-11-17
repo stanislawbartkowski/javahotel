75882bc2fda36da869fac342d2b40e182b987aa7
stanislawbartkowski@gmail.com <stanislawbartkowski@gmail.com@fcd2c1aa-8a59-11de-b841-6521bc8c149b>
2012-11-17 10:07:55 +0000

    Change related to presentation edit cell

--- gwtmodel/src/com/gwtmodel/table/view/table/PresentationEditCellFactory.java
+++ gwtmodel/src/com/gwtmodel/table/view/table/PresentationEditCellFactory.java
@@ -15,17 +15,14 @@ package com.gwtmodel.table.view.table;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.gwt.cell.client.AbstractEditableCell;
 import com.google.gwt.cell.client.ActionCell;
 import com.google.gwt.cell.client.Cell;
-import com.google.gwt.cell.client.CheckboxCell;
 import com.google.gwt.cell.client.ClickableTextCell;
 import com.google.gwt.cell.client.CompositeCell;
 import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.cell.client.HasCell;
 import com.google.gwt.cell.client.SelectionCell;
 import com.google.gwt.cell.client.ValueUpdater;
-import com.google.gwt.dom.client.BrowserEvents;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.safehtml.shared.SafeHtml;
@@ -105,82 +102,6 @@ class PresentationEditCellFactory extends PresentationEditCellHelper {
                 eCol, iStartEdit);
     }
 
-    // Decorator pattern implemented to add "blur" event to the constructor
-    // Cannot inherit CheckboxCell directly
-    private class EditCheckBoxCell extends
-            AbstractEditableCell<Boolean, Boolean> implements IGetField {
-
-        private final IVField v;
-        private final CheckboxCell checkBox;
-
-        EditCheckBoxCell(IVField v, boolean handleSelection) {
-            super(BrowserEvents.CHANGE, BrowserEvents.KEYDOWN,
-                    BrowserEvents.BLUR);
-            this.checkBox = new CheckboxCell(false, handleSelection);
-            this.v = v;
-        }
-
-        @Override
-        public boolean dependsOnSelection() {
-            return checkBox.dependsOnSelection();
-        }
-
-        @Override
-        public boolean handlesSelection() {
-            return checkBox.handlesSelection();
-        }
-
-        @Override
-        public Object getValObj(MutableInteger key) {
-            Boolean b = checkBox.getViewData(key);
-            return b;
-        }
-
-        @Override
-        public void setValObj(MutableInteger key, Object o) {
-            Boolean b = (Boolean) o;
-            checkBox.setViewData(key, b);
-        }
-
-        @Override
-        public boolean isEditing(
-                com.google.gwt.cell.client.Cell.Context context,
-                Element parent, Boolean value) {
-            return checkBox.isEditing(context, parent, value);
-        }
-
-        @Override
-        public void onBrowserEvent(Context context, Element parent,
-                Boolean value, NativeEvent event,
-                ValueUpdater<Boolean> valueUpdater) {
-            String type = event.getType();
-            checkBox.onBrowserEvent(context, parent, value, event, valueUpdater);
-            afterChange(type, context, v);
-        }
-
-        @Override
-        public void render(Context context, Boolean value, SafeHtmlBuilder sb) {
-            Object key = context.getKey();
-            MutableInteger i = (MutableInteger) key;
-            boolean editenabled = eCol.isEditable(i.intValue(), v);
-            if (editenabled) {
-                checkBox.render(context, value, sb);
-                return;
-            }
-
-            Boolean viewData = getViewData(key);
-            if (viewData != null && viewData.equals(value)) {
-                clearViewData(key);
-                viewData = null;
-            }
-            if (value) {
-                sb.append(INPUT_CHECKED);
-            } else {
-                sb.append(INPUT_UNCHECKED);
-            }
-
-        }
-    }
 
     private class EditSelectionCell extends SelectionCell implements IGetField {
 
@@ -260,10 +181,6 @@ class PresentationEditCellFactory extends PresentationEditCellHelper {
         return dFactory.constructDateEditCol(he);
     }
 
-//    @SuppressWarnings("rawtypes")
-//    Column contructBooleanCol(IVField v, boolean handleSelection) {
-//        return new CheckBoxColumn(new EditCheckBoxCell(v, handleSelection), v);
-//    }
     
     @SuppressWarnings("rawtypes")
     Column contructBooleanCol(IVField v, boolean handleSelection) {
