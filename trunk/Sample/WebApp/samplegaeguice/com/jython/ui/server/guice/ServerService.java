5749a6c66ee257c0fa87d2f202633e092aa1b3ef
stanislawbartkowski@gmail.com <stanislawbartkowski@gmail.com@fcd2c1aa-8a59-11de-b841-6521bc8c149b>
2013-03-24 23:04:31 +0000

    Changes related to server side properties

--- trunk/Sample/WebApp/samplegaeguice/com/jython/ui/server/guice/ServerService.java
+++ trunk/Sample/WebApp/samplegaeguice/com/jython/ui/server/guice/ServerService.java
@@ -12,18 +12,18 @@
  */
 package com.jython.ui.server.guice;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.gwtmodel.commoncache.CommonCacheFactory;
 import com.gwtmodel.commoncache.ICommonCache;
 import com.jython.ui.server.Cached;
 import com.jython.ui.server.datastore.IPersonOp;
 import com.jython.ui.server.datastore.gae.PersonOp;
-import com.jythonui.server.IJythonUIServer;
+import com.jythonui.server.IJythonClientRes;
 import com.jythonui.server.IJythonUIServerProperties;
-import com.jythonui.server.JythonUiServerProvider;
+import com.jythonui.server.defa.GetClientProperties;
 import com.jythonui.server.defa.IsCached;
 import com.jythonui.server.defa.ServerProperties;
+import com.jythonui.server.guice.JythonServerService.JythonServiceModule;
 import com.jythonui.server.service.Holder;
 
 /**
@@ -32,17 +32,18 @@ import com.jythonui.server.service.Holder;
  */
 public class ServerService {
 
-    public static class ServiceModule extends AbstractModule {
+    public static class ServiceModule extends JythonServiceModule {
+
         @Override
         protected void configure() {
+            configureJythonUi();
             bind(IsCached.class).to(Cached.class).in(Singleton.class);
             bind(IPersonOp.class).to(PersonOp.class).in(Singleton.class);
             bind(IJythonUIServerProperties.class).to(ServerProperties.class)
                     .in(Singleton.class);
-            bind(IJythonUIServer.class)
-                    .toProvider(JythonUiServerProvider.class).in(
-                            Singleton.class);
             bind(ICommonCache.class).toProvider(CommonCacheFactory.class).in(
+                    Singleton.class);
+            bind(IJythonClientRes.class).to(GetClientProperties.class).in(
                     Singleton.class);
             requestStaticInjection(Holder.class);
         }
--- trunk/Sample/WebApp/samplewebsrc/com/jython/ui/client/WebJythonUi.java
+++ trunk/Sample/WebApp/samplewebsrc/com/jython/ui/client/WebJythonUi.java
@@ -1,42 +1,15 @@
 package com.jython.ui.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.gwtmodel.table.factories.ITableAbstractFactories;
-import com.gwtmodel.table.injector.GwtGiniInjector;
-import com.gwtmodel.table.injector.WebPanelHolder;
-import com.gwtmodel.table.view.webpanel.IWebPanel;
-import com.gwtmodel.table.view.webpanel.WebPanelFactory;
-import com.jythonui.client.IJythonUIClient;
-import com.jythonui.client.JythonUIClientFactory;
-import com.jythonui.client.service.JResource;
+import com.jythonui.client.JythonClientStart;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class WebJythonUi implements EntryPoint {
 
-    private final IJythonUIClient iClient;
-    private static final String START = "start.xml";
-
-    public WebJythonUi() {
-        iClient = JythonUIClientFactory.construct(new JResource());
-    }
-
     public void onModuleLoad() {
-
-        startW();
-        iClient.start(START);
+        JythonClientStart.start(null);
     }
 
-    private void startW() {
-        ITableAbstractFactories tFactories = GwtGiniInjector.getI()
-                .getITableAbstractFactories();
-        tFactories.registerWebPanelResources(new WebPanelResources());
-
-        WebPanelFactory wFactory = GwtGiniInjector.getI().getWebPanelFactory();
-        IWebPanel wPan = wFactory.construct(null);
-        WebPanelHolder.setWebPanel(wPan);
-        RootPanel.get().add(wPan.getWidget());
-    }
 }
