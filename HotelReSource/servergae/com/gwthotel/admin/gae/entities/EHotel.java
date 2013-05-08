b6a2fd4d3013a5c03c5932504bde47e3dd6ff6c6
stanislawbartkowski <stanislawbartkowski@fcd2c1aa-8a59-11de-b841-6521bc8c149b>
2013-05-08 22:49:13 +0000

    Removed HotelReSource

--- HotelReSource/servergae/com/gwthotel/admin/gae/entities/EHotel.java
+++
@@ -1,8 +0,0 @@
-package com.gwthotel.admin.gae.entities;
-
-import com.googlecode.objectify.annotation.EntitySubclass;
-
-@EntitySubclass(index = true)
-public class EHotel extends EDictionary {
-
-}
