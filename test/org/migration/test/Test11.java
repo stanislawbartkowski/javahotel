165289a646a75505753854518b01cc9883c47cf5
stanislawbartkowski <stanislawbartkowski@gmail.com>
2017-07-05 23:20:01 +0200

    tests

---
+++ test/org/migration/test/Test11.java
@@ -0,0 +1,16 @@
+package org.migration.test;
+
+import org.junit.Test;
+import org.migration.tasks.ProcList;
+
+public class Test11 extends TestHelper {
+	
+	private static String IFILE="/home/sbartkowski/Dokumenty/db2/eije/salida_ETXE01D/DBanalysisIVAP01D.package.txt"; 
+	
+	@Test
+	public void test1() throws Exception {
+		ProcList.stat(IFILE, true);
+
+	}
+
+}
